8b58491ffcd7fac5693a1eff062d15a585b097dd
Ostapenlo Andrei <ostapdans99@gmail.com>
2020-12-24 14:12:53 +0300

    feat: added solution for task1 and task2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(10)
+function myForEach (array, callback, arguments) {
+    for(let index = 0; index < array.length; index++){
+        callback.call(arguments, array[index], index, array)
+    }
+}
+
+
+function createDebounceFunction (callback, delay) {
+    let interval;
+        return (...args) => {
+            clearTimeout(interval);
+            interval = setTimeout(() => {
+                interval = null
+                callback(...args)
+            }, delay)
+    }
+}
+
+
+
+
+
